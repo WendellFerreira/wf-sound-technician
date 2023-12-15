b37c5e28f4b74e5e4ff304f7cf18bc5d6d3b9d67
WendellFerreira <86381991+WendellFerreira@users.noreply.github.com>
2023-12-15 14:06:13 -0300

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,14 @@
+let wfMenu = document.getElementById('wf-menu')
+let menu = document.getElementById('menu-mobile')
+let overlay = document.getElementById('overlay-menu')
+
+wfMenu.addEventListener('click', ()=>{
+    menu.classList.add('abrir-menu')
+})
+
+menu.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
+overlay.addEventListener('click', ()=>{
+    menu.classList.remove('abrir-menu')
+})
